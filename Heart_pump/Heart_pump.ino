e561e9214559c25c218c55fbe56e62fd0c70ab94
Rada Berar <ujagaga@gmail.com>
2025-05-04 17:31:21 +0200

    Latest

--- Heart_pump/Heart_pump.ino
+++ Heart_pump/Heart_pump.ino
@@ -1,43 +1,34 @@
-#include <ssd1306.h>
 #include <EEPROM.h>
-#include <SoftwareSerial.h>
 
 
-#define SERVO1PIN     1
-#define SERVO2PIN     2
-#define UART_TX_PIN   255
-#define UART_RX_PIN   0
-#define OLED_SCL_PIN  3
-#define OLED_SDA_PIN  4
+#define SERVO1PIN     5
+#define SERVO2PIN     6
+#define BTNPIN        8
+#define LEDPIN        7
 
 /* Servo pulse duration 1000us to 2000us */
-#define MIN_ANGLE   1200
-#define MAX_ANGLE   1800
-#define MAX_SPEED   100
-#define EEPROM_ADDR 2
+#define MIN_ANGLE       1200
+#define MAX_ANGLE       1800
+#define MAX_SPEED       50
+#define EEPROM_ADDR     2
+#define SHUTDOWN_COUNT  30
 
 #define STATE_OFF           0
 #define STATE_UP_CONTRACT   1
 #define STATE_UP_EXPAND     2
 #define STATE_DOWN_CONTRACT 3
 #define STATE_DOWN_EXPAND   4
-#define SCREEN_OFFSET       32
-#define IMG_START           40
-#define IMG_TRAIL           70
-#define CMD_TIMEOUT         100
-#define CMD_START           0xA5
 
 uint32_t angle1 = MIN_ANGLE;
 uint32_t angle2 = MIN_ANGLE;
 uint8_t speed = 5;    
 bool pwr_flag = false;
 uint8_t h_state = STATE_OFF;
-uint8_t cursorx = 0;
-uint8_t yoffset;
 uint32_t cmd_timestamp = 0;
+uint32_t led_timestamp = 0;
+uint8_t cmd_count = 0;
 uint32_t servo_sync_timestamp = 0;
 
-SoftwareSerial mySerial(UART_RX_PIN, UART_TX_PIN);
 
 void servo_delay_short(uint32_t duration_us){ 
   while(duration_us > 500){
@@ -66,119 +57,71 @@ void update_servos(){
   digitalWrite(SERVO2PIN, HIGH);
   servo_delay_short(angle2);
   digitalWrite(SERVO2PIN, LOW);
+} 
+
+void LED_set(){
+  led_timestamp = millis();
+  digitalWrite(LEDPIN, HIGH);
 }
 
-void draw_ecg(){
-  uint8_t y;
-  uint8_t x = cursorx & 127; 
-  uint8_t d = (cursorx - IMG_TRAIL) & 127;
-
-  if(x < IMG_START){     
-    yoffset = 0;
-  }else if(x < (IMG_START+2)){
-    yoffset += 2;   
-  }else if(x < (IMG_START+3)){
-    yoffset += 1;   
-  }else if(x < (IMG_START+5)){ 
-
-  }else if(x < (IMG_START+6)){
-    yoffset -= 1;     
-  }else if(x < (IMG_START+8)){
-    yoffset -= 2;  
-  }else if(x < (IMG_START+14)){
-
-  }else if(x < (IMG_START+15)){
-    yoffset -= 3;  
-  }else if(x < (IMG_START+20)){
-    yoffset += 6;  
-  }else if(x < (IMG_START+26)){
-    yoffset -= 6;  
-  }else if(x < (IMG_START+27)){
-    yoffset += 6;  
-  }else if(x < (IMG_START+28)){
-    yoffset += 3;  
-  }else if(x < (IMG_START+32)){ 
-
-  }else if(x < (IMG_START+34)){
-    yoffset += 2;   
-  }else if(x < (IMG_START+35)){
-    yoffset += 1;   
-  }else if(x < (IMG_START+37)){ 
-
-  }else if(x < (IMG_START+38)){
-    yoffset -= 1;     
-  }else if(x < (IMG_START+40)){
-    yoffset -= 2;  
-  }
-
-  y = SCREEN_OFFSET + yoffset;
-  uint8_t clear_buff[16] = {0};
-
-  ssd1306_drawVLine(x, y, y+3);
-  ssd1306_drawBuffer(d, 0, 2, 64, clear_buff);   
-  cursorx++; 
+void LED_process(){
+  if((led_timestamp > 0) && ((millis() - led_timestamp) > 100)){
+    led_timestamp = 0;
+    digitalWrite(LEDPIN, LOW);
+  }  
 }
 
-void draw_screensaver(){
-  ssd1306_fillScreen(0x00); 
-  int x, y, r, dist;
-  r = 16;
-  
-  for (x = 0; x <= r*2; x++){
-    for (y = 0; y <= r * 2; y++){
-      dist = sqrt((r-x) * (r-x) + (r-y) * (r-y));
-    
-      if (dist==r){
-        ssd1306_drawVLine(x+64-r, y+SCREEN_OFFSET-r, y+SCREEN_OFFSET-r+2);
-      }          
-    }   
-    
-  }
-  ssd1306_drawHLine(64-r, SCREEN_OFFSET, 64);
-  ssd1306_drawHLine(64-r, SCREEN_OFFSET+1, 64);
-  ssd1306_drawHLine(64-r, SCREEN_OFFSET-1, 64);
-}
+void btn_cmd(){
 
-uint8_t cmd_rx(){  
-  uint8_t rx = 0;
-  bool rx_received = false;  
-  uint8_t last_rx = 0;
+  if(digitalRead(BTNPIN) == LOW){  
+    cmd_timestamp = millis();
 
-  while(mySerial.available()) {
-    rx = mySerial.read();
+    delay(50);
+    if(cmd_count <= SHUTDOWN_COUNT){
+      cmd_count++;
+      digitalWrite(LEDPIN, HIGH);     
+      led_timestamp = millis();
+    }else{
+      pwr_flag = false;
+      digitalWrite(LEDPIN, LOW);
+    } 
+  }else{
+    if(cmd_count >= SHUTDOWN_COUNT){
+      
+      EEPROM.begin();
+      EEPROM.update(EEPROM_ADDR, speed);
+      EEPROM.end();
+    }else{
+      uint32_t btn_press_timespan = millis() - cmd_timestamp;
 
-    if(rx == CMD_START){
-      last_rx = rx;
-    }else if(last_rx == CMD_START){
-      rx_received = true;
-      last_rx = rx;
-    }         
-  }
-
-  if(rx_received){ 
-    speed = rx;
-
-    if(speed > MAX_SPEED){
-      speed = MAX_SPEED;
+      if(cmd_timestamp > 0){
+        if(btn_press_timespan < (SHUTDOWN_COUNT * 50)){
+          if(pwr_flag){
+            speed+=5;
+            if(speed > MAX_SPEED ){
+              speed = 5;
+            }  
+          }else{        
+            pwr_flag = true;     
+          }     
+        }      
+      }  
     }
-
-    EEPROM.begin();
-    EEPROM.update(EEPROM_ADDR, speed);
-    EEPROM.end();  
-    // mySerial.write(speed);        
-  }    
+    
+    cmd_count = 0;
+    cmd_timestamp = 0;  
+  }
 }
-   
+
 void setup() {
-  pinMode(UART_RX_PIN, INPUT); 
   pinMode(SERVO1PIN, OUTPUT); 
-  pinMode(SERVO2PIN, OUTPUT); 
+  pinMode(SERVO2PIN, OUTPUT);
+  pinMode(LEDPIN, OUTPUT); 
+  pinMode(BTNPIN, INPUT_PULLUP);
   digitalWrite(SERVO1PIN, LOW);
   digitalWrite(SERVO2PIN, LOW);   
-
-  ssd1306_128x64_i2c_initEx(3,4,0);
-  ssd1306_fillScreen(0x00);  
-  draw_screensaver();
+  digitalWrite(LEDPIN, LOW);
+  
 
   EEPROM.begin();
   speed = EEPROM.read(EEPROM_ADDR);
@@ -189,22 +132,27 @@ void setup() {
     speed = 5;
   }  
 
-  mySerial.begin(9600);
+  Serial.begin(9600);
+  Serial.print("\n\nSpeed:");
+  Serial.println(speed);
 }
 
 void loop()  { 
-  cmd_rx();
-  pwr_flag = (digitalRead(UART_RX_PIN) == HIGH);  
+  btn_cmd();
+  LED_process();
+
 
   if(!pwr_flag){ 
     if(h_state != STATE_OFF){
       h_state = STATE_OFF;
-      draw_screensaver();     
+      Serial.println("Stop");
     }
   }else{
     if(h_state == STATE_OFF){
       h_state = STATE_UP_CONTRACT;
-      ssd1306_fillScreen(0x00); 
+
+      LED_set();
+      Serial.println("Start");
 
       EEPROM.begin();
       speed = EEPROM.read(EEPROM_ADDR);
@@ -215,31 +163,33 @@ void loop()  {
         speed = 5;
       }  
     }else{  
-      draw_ecg();    
-
       if(h_state == STATE_UP_CONTRACT){
         if(angle1 < MAX_ANGLE){
           angle1 += speed;              
         }else{
           h_state = STATE_UP_EXPAND;
+          LED_set();
         }
       }else if(h_state == STATE_UP_EXPAND){
         if(angle1 > MIN_ANGLE){
           angle1 -= speed;
         }else{
           h_state = STATE_DOWN_CONTRACT;
+          LED_set();
         }
       }else if(h_state == STATE_DOWN_CONTRACT){
         if(angle2 < MAX_ANGLE){
           angle2 += speed;
         }else{
           h_state = STATE_DOWN_EXPAND;
+          LED_set();
         }
       }else if(h_state == STATE_DOWN_EXPAND){
         if(angle2 > MIN_ANGLE){
           angle2 -= speed;          
         }else{
-          h_state = STATE_UP_CONTRACT;            
+          h_state = STATE_UP_CONTRACT;  
+          LED_set();          
         }
       }
 
